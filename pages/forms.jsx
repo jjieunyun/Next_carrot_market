485ef9bec60bd7d3f40795f4f9293697f19175e1
jieun <lesson0186@gmail.com>
2022-10-18 18:35:07 +0900

    React-Hook-Form/ Finished

--- pages/forms.jsx
+++ pages/forms.jsx
@@ -1,22 +1,56 @@
 import React,{useState} from 'react';
 import {useForm} from "react-hook-form";
 
-// Less code,
-// better Validation
 //Better Errors
 //Have control over inputs
 //Don't Deal with Events
 //Easier inputs
 
 function Forms(props) {
-  const {register, watch} = useForm()
+  const {
+    register, watch, handleSubmit, formState : {errors},reset
+  } = useForm({
+    mode: "onChange"
+  })
+  const onValid = () => {
+    console.log('valid')
+    reset()
+  }
+  const onInvalid = (errors) => {
+    console.log(errors)
+  }
 
+  console.log(errors)
 
   return (
-    <form >
-      <input {...register("username")} type="text" placeholder='UserName' required/>
-      <input {...register('email')}  type="email" placeholder="Email" required/>
-      <input {...register('password')} type="password" placeholder="Password" required/>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input {
+        ...register("username",
+          {
+            required : "UserName is required",
+            minLength : {
+              message : "5글자 이상 되어야됩니다.",
+              value : 5
+            }
+          })}
+             type="text" placeholder='UserName' />
+      {errors.email?.message}
+      <input {
+        ...register('email',
+          {
+            required : "Email is required",
+            validate : {
+              notGmail : (value) => !value.includes("@gmail.com") || "Gmail은 사용할 수 없습니다."
+            }
+          })}
+             type="email" placeholder="Email"
+              className={`${Boolean(errors.email?.message)? " border-red-500":""}`}/>
+      <input {
+        ...register('password',
+          {required : "Password is required",
+            minLength : 8
+          })}
+             type="password" placeholder="Password" />
       <input type="submit" value='Create Account'/>
     </form>
   );
